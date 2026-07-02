034aa3551a7d488f6e44507a6f696e7071dfabd8
Omwanda Gerald <omwandagerald7@gmail.com>
2026-07-02 10:11:57 +0300

    Remove dotenv dependency for Render compatibility

--- mybookstore/settings.py
+++ mybookstore/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,6 +54,8 @@ INSTALLED_APPS = [
     'django_pesapal',
     'cloudinary_storage',
    ]
+
+import os
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'mfho6mc1',
