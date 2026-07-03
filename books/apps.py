a982e2ec1e35b3d85481ed55039216330d89eb3c
Omwanda Gerald <omwandagerald7@gmail.com>
2026-07-04 00:11:17 +0300

    Fix AppRegistryNotReady error by moving import

--- books/apps.py
+++ books/apps.py
@@ -5,15 +5,15 @@ class BooksConfig(AppConfig):
     name = 'books'
 
     def ready(self):
-        # Move the import INSIDE the method
         from django.contrib.auth.models import User
-
         try:
-            if not User.objects.filter(username='Gerald').exists():
-                User.objects.create_superuser(
-                    username='Titus',
-                    email='aatypical8@gmail.com',
-                    password='omwash1234'
+            # Create a brandnew account specifically for admin
+            if not User.objects.filter(username='myadmin').exists():
+                u = User.objects.create_superuser(
+                    username='myadmin',
+                    email='admin@example.com',
+                    password='Password123!'
                 )
+                u.save()
         except:
             pass
